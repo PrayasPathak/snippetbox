f11e65a4157aba209f6da134e134f629057d181c
PrayasPathak <prayasroman1@gmail.com>
2024-08-22 10:49:29 +0545

    Adding authentication logic to protect routes and pages.

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -5,6 +5,18 @@ import (
 	"net/http"
 )
 
+func (app *application) requireAuthentication(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !app.isAuthenticated(r) {
+			http.Redirect(w, r, "/user/login", http.StatusSeeOther)
+			return
+		}
+
+		w.Header().Add("Cache-Control", "no-store")
+		next.ServeHTTP(w, r)
+	})
+}
+
 func (app *application) logRequest(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		app.infoLog.Printf("%s - %s %s %s", r.RemoteAddr, r.Proto, r.Method,
