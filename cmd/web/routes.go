0e2e10476c9cf9e29f05c347795dd7a323f3d16d
PrayasPathak <prayasroman1@gmail.com>
2024-08-24 12:58:19 +0545

    Added about page and debug mode

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -23,6 +23,7 @@ func (app *application) routes() http.Handler {
 	// unprotected application routes (that do not require user authentication)
 	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
 	router.Handler(http.MethodGet, "/", dynamic.ThenFunc(app.home))
+	router.Handler(http.MethodGet, "/about", dynamic.ThenFunc(app.about))
 	router.Handler(http.MethodGet, "/snippet/view/:id", dynamic.ThenFunc(app.snippetView))
 	router.Handler(http.MethodGet, "/user/signup", dynamic.ThenFunc(app.userSignup))
 	router.Handler(http.MethodPost, "/user/signup", dynamic.ThenFunc(app.userSignupPost))
